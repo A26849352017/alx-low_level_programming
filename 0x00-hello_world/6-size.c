d06c64dcd5fd41a9a331ca6cbd49a0827e89d6f8
A26849352017 <phelixoyugi8@gmail.com>
2022-06-03 02:45:38 -0700

    C programm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,19 @@
 /**
  * main -prints a string that in pups the function
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Successi)
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
-	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
-	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+	char a;
+	int b;
+	int long c;
+	int long long d;
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
 
