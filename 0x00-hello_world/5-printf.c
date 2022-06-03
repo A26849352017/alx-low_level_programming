6cc17fce4a25948c5c86062779915c48c45a1309
A26849352017 <phelixoyugi8@gmail.com>
2022-06-03 01:16:58 -0700

    C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - prints a string that in pups the functions
-* Description: Printing with proper grammer, but the outcome is a piece of art"
+* main - prints the phrase with proper grammar,
+* but the outcome is a piece of art"
 * Return: Always 0 (Success)
 */
 
 int main(void)
 {
-		printf("\"With proper grammar, but the outcome is a piece of art,\n");
+		printf("With proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
