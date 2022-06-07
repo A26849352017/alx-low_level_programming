00ffb5a1976399670e9447a3de95aa9703cffe5a
A26849352017 <phelixoyugi8@gmail.com>
2022-06-07 13:21:51 -0700

    Writes a program that prints a file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include "main.h"
 /**
- * main - main function
+ * main -prints out _putchar followed by a newline.
  *
- * Return: End program
+ * Return: always (0)
  */
 int main(void)
 {
