1b8c536a111aae9b59e6e09507a65ebfaf5bef3e
A26849352017 <phelixoyugi8@gmail.com>
2022-06-07 04:55:47 -0700

    Write a program that printd file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,20 +10,19 @@ int main(void)
 	int b = 0;
 	int c = 0;
 	int coma = 1;
-
 	while (a <= 9)
 	{
 		while (b <= 9)
-		{
-			while (c <= 9)
+		 {
+			 while (c <= 9)
 			{
 				if (a < b && b < c)
 				{
 					if (coma == 0)
-					{
-						putchar(',');
-						putchar(32);
-					}
+					 {
+						 putchar(',');
+						 putchar(32);
+					 }
 					coma = 0;
 					putchar(a + '0');
 					putchar(b + '0');
@@ -33,11 +32,10 @@ int main(void)
 			}
 			++b;
 			c = 0;
-		}
+			 }
 		++a;
-		b = 0;
+		b = 0
 	}
 	putchar(10);
-
 	return (0);
 }
